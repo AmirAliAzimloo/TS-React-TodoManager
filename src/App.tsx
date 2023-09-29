4f5addfbd50036c9dd769035cda9c3f36322c8d9
Amir Ali <amiraliazimloo123@gmail.com>
2023-09-29 15:01:07 +0330

    config project & install needed pakages completed

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Test from './Test'
+
+function App() {
+
+  return (
+    <>
+    <Test/>
+    <br />
+     <h1>
+      Hello World 💚😉
+     </h1>
+    </>
+  )
+}
+
+export default App
