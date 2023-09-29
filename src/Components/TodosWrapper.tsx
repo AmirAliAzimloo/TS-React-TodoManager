3e8c86a38d7fef44d07b6d8b25ffcf8385d27e57
Amir Ali <amiraliazimloo123@gmail.com>
2023-09-29 18:56:23 +0330

    project finish

--- src/Components/TodosWrapper.tsx
+++ src/Components/TodosWrapper.tsx
@@ -41,7 +41,6 @@ function TodosWrapper() {
 
   const toggleComplete = (id: string) => {
     setTodos(todos.map(todo=>todo.id == id ? {...todo,completed : !todo.completed } : todo))
-    return true
   };
 
   return (
